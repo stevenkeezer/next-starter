a6fbca0eb8106753bfbbf187d6dc1f7d3a235bf0
Steven Keezer <stevengkeezer@gmail.com>
2024-04-28 23:15:00 -0700

    fix:rm auth shit

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,14 +1,14 @@
 import Link from 'next/link';
 import { getServerSession } from 'next-auth';
 
-import { authOptions } from '@/app/api/auth/[...nextauth]/auth-options';
+
 import { SignInButton } from '@/components/navbar/sign-in-button';
 import { ThemeToggle } from '@/components/navbar/theme-toggle';
 import { UserDropdown } from '@/components/navbar/user-dropdown';
 import Image from 'next/image';
 
 export const Navbar = async () => {
-  const session = await getServerSession(authOptions);
+
 
   return (
     <header className="w-full bg-[#d9edfd]">
@@ -22,7 +22,7 @@ export const Navbar = async () => {
         </Link>
         <div className="flex items-center gap-2">
           <ThemeToggle />
-          {session ? <UserDropdown session={session} /> : <SignInButton />}
+           <SignInButton />
         </div>
       </div>
     </header>
