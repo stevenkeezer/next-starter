5e19f367273f198df89a64d33d715c6a0e617353
Steven Keezer <stevengkeezer@gmail.com>
2024-05-31 00:50:15 -0700

    fix: moal

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -16,10 +16,10 @@ export default function MainNav() {
       <div className="relative flex h-16 justify-between max-w-[66rem] mx-auto">
         <div className="relative z-10 flex">
           <div className="hidden lg:flex flex-shrink-0 items-center">
-            <div className="flex items-center gap-1 uppercase text-sky-900/80 text-sm tracking-wider font-bold space-x-16  mr-9">
-              <div onClick={() => handleScroll('how-it-works')} style={{ cursor: 'pointer' }}>How it works</div>
-              <div onClick={() => handleScroll('portfolio')} style={{ cursor: 'pointer' }}>Portfolio</div>
-              <div onClick={() => handleScroll('about')} style={{ cursor: 'pointer' }}>About</div>
+            <div className="flex items-center gap-1 uppercase text-sky-900/90 text-sm tracking-wide font-semibold space-x-16">
+              <div onClick={() => handleScroll('how-it-works')} style={{ cursor: 'pointer' }} className='hover:underline'>How it works</div>
+              <div onClick={() => handleScroll('portfolio')} style={{ cursor: 'pointer' }} className='hover:underline'>Portfolio</div>
+              <div onClick={() => handleScroll('about')} style={{ cursor: 'pointer' }} className='hover:underline'>About</div>
             </div>
           </div>
         </div>
@@ -29,7 +29,7 @@ export default function MainNav() {
               Search
             </label>
             <div className="relative">
-              <Link href="/" className="flex items-center text-lg text-blue-400 font-bold justify-center">
+              <Link href="/" className="flex items-center text-lg text-blue-400 font-semibold justify-center">
                 <Image src="/no-color.svg" alt="Golden State Web Design" className='opacity-90 drop-shadow-lg' width={140} height={140} />
               </Link>
             </div>
@@ -37,11 +37,11 @@ export default function MainNav() {
         </div>
 
         <div className="hidden lg:relative lg:z-10 lg:flex lg:items-center">
-          <div className="flex items-center gap-1 uppercase text-sky-900/80 text-sm tracking-wider font-bold space-x-16  ml-8">
-            <div onClick={() => handleScroll('testimonials')} style={{ cursor: 'pointer' }}>Testimonials</div>
-            <div onClick={() => handleScroll('pricing')} style={{ cursor: 'pointer' }}>Pricing</div>
+          <div className="flex items-center gap-1 uppercase text-sky-900/90 text-sm tracking-wide font-semibold space-x-16">
+            <div onClick={() => handleScroll('testimonials')} style={{ cursor: 'pointer' }} className='hover:underline'>Testimonials</div>
+            <div onClick={() => handleScroll('pricing')} style={{ cursor: 'pointer' }} className='hover:underline'>Pricing</div>
             {/* <SignInButton /> */}
-            <Link href="/?open=dialog">
+            <Link href="/?open=dialog" className='hover:underline'>
               <div>Contact</div>
             </Link>
           </div>
