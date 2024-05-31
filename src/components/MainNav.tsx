428aa42aaa5ceacdb3db8045e7ee4e3b5c81d091
Steven Keezer <stevengkeezer@gmail.com>
2024-05-31 00:42:29 -0700

    fix: moal

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -30,7 +30,7 @@ export default function MainNav() {
             </label>
             <div className="relative">
               <Link href="/" className="flex items-center text-lg text-blue-400 font-bold justify-center">
-                <Image src="/no-color.svg" alt="Golden State Web Design" className='opacity-90' width={143} height={143} />
+                <Image src="/no-color.svg" alt="Golden State Web Design" className='opacity-90 drop-shadow-lg' width={140} height={140} />
               </Link>
             </div>
           </div>
