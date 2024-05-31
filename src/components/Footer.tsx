1fe4e5f1b182e049cd92a5f4f640139b8883409b
Steven Keezer <stevengkeezer@gmail.com>
2024-05-30 23:05:25 -0700

    fix: moal

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -102,11 +102,11 @@ export function Footer() {
           ))}
         </div> */}
         <div className="mt-16 border-t border-white/10 pt-8 sm:mt-20 lg:mt-24">
-          <div className="text-xs leading-5 text-white/20">
+          <div className="text-xs leading-5 text-white/40">
             <p>&copy; {new Date().getFullYear()} Golden State Web Design, Inc. All rights reserved.</p>
             <div className="mt-4 text-xs">
               <p className="mt-2 text-xs">
-                Serving: El Dorado Hills, Folsom, Granite Bay, Cameron Park, Shingle Springs, Placerville, Rescue, Orangevale, Fair Oaks, Rancho Cordova, Roseville, Rocklin, Lincoln, Auburn, Citrus Heights, Carmichael, Antelope, Sacramento, North Highlands, Loomis, Penryn, Newcastle, Elverta, Rio Linda, McClellan Park, Arden-Arcade, Gold River, Vineyard, Wilton, Rancho Murieta, Herald, Galt, Elk Grove, Walnut Grove, Isleton, Clarksburg, Courtland, West Sacramento, Davis, Woodland, Knights Landing, Yuba City, Marysville, Wheatland, Nicolaus, Rio Oso, Robbins, Sutter, Live Oak, Olivehurst, Plumas Lake, Linda, East Nicolaus, Trowbridge, Meridian, Colusa, Williams, Arbuckle, Grimes, Maxwell, Princeton, Stonyford, Sites, College City, Dunnigan, Zamora, Dobbins, Oregon House, Rackerby, Brownsville, Forbestown, Challenge, Bangor, Loma Rica, Smartsville, Penn Valley, Rough and Ready, Lake Wildwood, Nevada City, Grass Valley, Alta Sierra, Chicago Park, Cedar Ridge, Peardale, Gold Run, Dutch Flat, Alta, Blue Canyon, Emigrant Gap, Baxter, and more.
+                Serving: El Dorado Hills, Folsom, Granite Bay, Cameron Park, Shingle Springs, Placerville, Rescue, Orangevale, Fair Oaks, Rancho Cordova, Roseville, Rocklin, Lincoln, Auburn, Citrus Heights, Carmichael, Antelope, Sacramento, North Highlands, Loomis and more.
               </p>
             </div>
           </div>
