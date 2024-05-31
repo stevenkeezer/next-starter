e555d03fef205a360c38c2050ae9fddfe5682ef0
Steven Keezer <stevengkeezer@gmail.com>
2024-05-30 19:55:48 -0700

    fi

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || '',
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://www.goldenstatewebdesign.com',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
