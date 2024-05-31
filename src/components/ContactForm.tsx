4dc920cfd036929f11ba7ed13498b094e8dfeb17
Steven Keezer <stevengkeezer@gmail.com>
2024-05-31 00:05:10 -0700

    fix: moal

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -8,6 +8,9 @@ import { Textarea } from './ui/textarea';
 import Image from 'next/image';
 import { DialogDescription } from '@radix-ui/react-dialog';
 import { Label } from '@radix-ui/react-dropdown-menu';
+import { useToast } from './ui/use-toast';
+
+import { Spinner } from './ui/spinner';
 
 export function ContactForm() {
     const router = useRouter();
@@ -16,6 +19,9 @@ export function ContactForm() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
+    const { toast } = useToast();
+    const [loading, setLoading] = useState(false);
+
 
     useEffect(() => {
         if (searchParams.get('open') === 'dialog') {
@@ -35,7 +41,7 @@ export function ContactForm() {
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        console.log(message, email, name, "ST/uf")
+        setLoading(true);
         try {
             const response = await fetch('/api/sendEmail', {
                 method: 'POST',
@@ -51,10 +57,14 @@ export function ContactForm() {
                 console.log('Email sent successfully', response);
                 // Handle success, e.g., show a success message or close the dialog
                 closeModal();
+                setLoading(false);
                 // reset form
+
                 setName('');
                 setEmail('');
                 setMessage('');
+                // create toast message
+                toast({ description: '🥳 Your email has been sent successfully! We will get back to you shortly.' })
             } else {
                 // Handle errors, e.g., show an error message
             }
@@ -68,8 +78,8 @@ export function ContactForm() {
             <DialogContent className="sm:max-w-[425px] overflow-y-auto items-start justify-start border-t-8 border-blue-400 h-full sm:h-auto bg-slate-50 text-slate-700">
                 <DialogHeader className='justify-center sm:flex hidden mx-auto mb-0 pb-0 '>
                     <div className='mx-auto'>
-                    <Image src="/no-color.svg" alt="Golden State Web Design" className='opacity-90 mb-10' width={143} height={143} />
-                </div>
+                        <Image src="/no-color.svg" alt="Golden State Web Design" className='opacity-90 mb-10' width={143} height={143} />
+                    </div>
                     <DialogTitle>Let's Connect!</DialogTitle>
                     <DialogDescription className='text-sm pt-6'>
                         We're excited to hear about your project! Please fill out the form below and we'll get back to you as soon as we can. Your thoughts and questions are important to us. Thank you!
@@ -78,14 +88,14 @@ export function ContactForm() {
 
                 <DialogHeader className='justify-center sm:hidden flex mx-auto mb-0 pb-0'>
                     <div className='mx-auto'>
-                    <Image src="/no-color.svg" alt="Golden State Web Design" className='opacity-90 mb-8 pl-2' width={123} height={123} />
-                </div>
+                        <Image src="/no-color.svg" alt="Golden State Web Design" className='opacity-90 mb-8 pl-2' width={123} height={123} />
+                    </div>
                     <DialogTitle>Let's Connect!</DialogTitle>
                     <DialogDescription className='text-xs pt-3'>
-                        We're excited to hear about your project! Please fill out the form below and we'll get back to you as soon as we can. 
+                        We're excited to hear about your project! Please fill out the form below and we'll get back to you as soon as we can.
                     </DialogDescription>
                 </DialogHeader>
-            
+
 
                 <form onSubmit={handleSubmit}>
                     <div className="grid gap-4 py-4 pt-10">
@@ -123,7 +133,10 @@ export function ContactForm() {
                         </div>
                     </div>
                     <DialogFooter className='pt-4'>
-                        <Button type="submit" variant="default" className='bg-blue-400 w-full text-white hover:bg-blue-300'>Send</Button>
+                        <Button type="submit" variant="default" className='bg-blue-400 w-full text-white hover:bg-blue-500'>
+                            {loading && <Spinner size="small" className='mr-2' />}
+                            {loading ? 'Sending...' : 'Send'}
+                        </Button>
                     </DialogFooter>
                 </form>
             </DialogContent>
