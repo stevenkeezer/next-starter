165cd21cab2fb3e3df4849784d0b25ff490a0f55
Steven Keezer <stevengkeezer@gmail.com>
2024-06-12 21:37:55 -0700

    fix: test

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -16,31 +16,22 @@ export default function MainNav() {
       <div className="relative flex h-20 justify-between max-w-[65rem] mx-auto ">
         <div className="relative z-10 flex">
           <div className="hidden lg:flex flex-shrink-0 items-center">
-            <div className="flex items-center gap-1 uppercase text-sky-900/80 text-sm tracking-wider font-semibold space-x-16">
+            <div className="flex items-center gap-1 uppercase text-blue-900/90 text-sm tracking-wider font-medium space-x-16">
               <div onClick={() => handleScroll('how-it-works')} style={{ cursor: 'pointer' }} className='hover:underline'>How it works</div>
               <div onClick={() => handleScroll('portfolio')} style={{ cursor: 'pointer' }} className='hover:underline'>Portfolio</div>
               <div onClick={() => handleScroll('about')} style={{ cursor: 'pointer' }} className='hover:underline'>About</div>
             </div>
           </div>
         </div>
-        <div className="hidden lg:flex z-0 flex-1 items-center justify-center px-2 sm:absolute sm:inset-0">
-          <div className="w-full sm:max-w-xs">
-            <label htmlFor="search" className="sr-only">
-              Search
-            </label>
-            <div className="relative">
-              <Link href="/" className="flex items-center text-lg text-blue-400 font-semibold justify-center">
-                <Image src="/no-color.svg" alt="Golden State Web Design" className='opacity-90 drop-shadow-sm' width={143} height={143} />
-              </Link>
-            </div>
-          </div>
+        <div className="hidden lg:flex flex-1 items-center justify-center">
+          <Link href="/" className="flex items-center text-lg text-blue-400 font-medium justify-center">
+            <Image src="/no-color.svg" alt="Golden State Web Design" className='opacity-90 drop-shadow-sm' width={143} height={143} />
+          </Link>
         </div>
-
-        <div className="hidden lg:relative lg:z-10 lg:flex lg:items-center">
-          <div className="flex items-center gap-1 uppercase text-sky-900/90 text-sm tracking-wide font-semibold space-x-16">
+        <div className="hidden lg:flex relative z-10 items-center">
+          <div className="flex items-center gap-1 uppercase text-blue-900/90 text-sm tracking-wide font-medium space-x-16">
             <div onClick={() => handleScroll('testimonials')} style={{ cursor: 'pointer' }} className='hover:underline'>Testimonials</div>
             <div onClick={() => handleScroll('pricing')} style={{ cursor: 'pointer' }} className='hover:underline'>Pricing</div>
-            {/* <SignInButton /> */}
             <Link href="/?open=dialog" className='hover:underline'>
               <div>Contact</div>
             </Link>
--- src/components/ProductReel.tsx
+++ src/components/ProductReel.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import React from "react";
 
 interface ProductReelProps {
   title: string;
@@ -9,6 +10,8 @@ interface ProductReelProps {
 }
 
 const ProductReel = (props: ProductReelProps) => {
+  const [isImageLoading, setImageLoading] = React.useState(true)
+
   return (
     <section className=" pb-20 pt-6 ">
       <div className="md:flex md:items-center px-4 sm:px-0 mb-4">
@@ -24,13 +27,16 @@ const ProductReel = (props: ProductReelProps) => {
         <div className="sm:mt-8 flex items-center w-full">
           <div className="w-full grid grid-cols-1 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-3 md:gap-y-6 lg:gap-x-10">
             <div className="h-[33rem] bg-slate-100 overflow-y-hidden sm:shadow-lg sm:border border-slate-200 p-4 sm:p-0">
-              <Image src="/jaco1.png" alt="Jaco Party Rentals" width={400} height={400} />
+              <Image src="/jaco1.png" alt="Jaco Party Rentals" width={400} height={400} onLoad={() => setImageLoading(false)}
+                className={`${isImageLoading ? 'blur' : 'remove-blur'}`} />
             </div>
             <div className="h-[33rem] bg-slate-100 overflow-y-hidden sm:shadow-lg sm:border border-slate-200 p-4 sm:p-0">
-              <Image src="/nepal.png" alt="Nepal Tea Trekking" width={400} height={400} />
+              <Image src="/nepal.png" alt="Nepal Tea Trekking" width={400} height={400} onLoad={() => setImageLoading(false)}
+                className={`${isImageLoading ? 'blur' : 'remove-blur'}`} />
             </div>
             <div className="h-[33rem] bg-slate-100 overflow-y-hidden sm:shadow-lg sm:border border-slate-200 p-4 sm:p-0">
-              <Image src="/sonoma.png" alt="sonoma county experience" width={400} height={400} />
+              <Image src="/sonoma.png" alt="sonoma county experience" width={400} height={400} onLoad={() => setImageLoading(false)}
+                className={`${isImageLoading ? 'blur' : 'remove-blur'}`} />
             </div>
           </div>
         </div>
