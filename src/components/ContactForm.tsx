fa0d28190fe655a72c7665856d63cb41970a7a7f
Steven Keezer <stevengkeezer@gmail.com>
2024-08-20 02:26:49 -0700

    fix

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Suspense } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
@@ -9,7 +9,6 @@ import Image from 'next/image';
 import { DialogDescription } from '@radix-ui/react-dialog';
 import { Label } from '@radix-ui/react-dropdown-menu';
 import { useToast } from './ui/use-toast';
-
 import { Spinner } from './ui/spinner';
 
 export function ContactForm() {
@@ -21,7 +20,6 @@ export function ContactForm() {
     const [message, setMessage] = useState('');
     const { toast } = useToast();
     const [loading, setLoading] = useState(false);
-
 
     useEffect(() => {
         if (searchParams.get('open') === 'dialog') {
@@ -51,20 +49,14 @@ export function ContactForm() {
                 body: JSON.stringify({ name, email, message }),
             });
 
-            //   console.log('hit', response)
-
             if (response.ok) {
                 console.log('Email sent successfully', response);
-                // Handle success, e.g., show a success message or close the dialog
                 closeModal();
                 setLoading(false);
-                // reset form
-
                 setName('');
                 setEmail('');
                 setMessage('');
-                // create toast message
-                toast({ description: '🥳 Your email has been sent successfully! We will get back to you shortly.' })
+                toast({ description: '🥳 Your email has been sent successfully! We will get back to you shortly.' });
             } else {
                 // Handle errors, e.g., show an error message
             }
@@ -74,72 +66,70 @@ export function ContactForm() {
     };
 
     return (
-        <Dialog open={isOpen} onOpenChange={closeModal}>
-            <DialogContent className="sm:max-w-[625px] overflow-y-auto items-start justify-start h-full sm:h-auto bg-white text-slate-700">
-                <DialogHeader className='justify-start sm:flex hidden ml-auto mb-0 pb-0 '>
-                    <div className='mr-auto mb-6 rounded-full -mt-3 pr-1 w-20 flex items-start justify-start h-20'>
-                    <Image src="/gs3.svg" alt="Golden State Web Design" className='opacity-90 hidden sm:flex' width={65} height={65} />
-                    </div>
-                    <DialogTitle>Let's Connect!</DialogTitle>
-                    <DialogDescription className='text-sm pt-3'>
-                        Let's connect! We're excited to hear about your project! Please fill out the form below and we'll get back to you as soon as we possible.
-                    </DialogDescription>
-                </DialogHeader>
-
-                <DialogHeader className='justify-center sm:hidden flex mx-auto pt-8 mb-0 pb-0'>
-                    <div className='mx-auto'>
-                    <Image src="/gs3.svg" alt="Golden State Web Design" className='opacity-90 lg:hidden ml-3 flex' width={55} height={55} />
-                    </div>
-                    <DialogTitle className='py-4 px-0'>How can we reach you?</DialogTitle>
-                    {/* <DialogDescription className='text-xs pt-3'>
-                        We're excited to hear about your project! Please fill out the form below and we'll get back to you as soon as we can.
-                    </DialogDescription> */}
-                </DialogHeader>
-
-
-                <form onSubmit={handleSubmit}>
-                    <div className="grid gap-5 py-4 pt-8">
-                        <div className="w-full">
-                            <Label className="text-slate-700 text-xs pb-3">Name<span className='text-red-500 pl-1'>*</span></Label>
-                            <Input
-                                id="name"
-                                placeholder="Your name"
-                                className="w-full bg-slate-50 border-slate-300"
-                                autoComplete='name'
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                            />
+        <Suspense fallback={<div>Loading...</div>}>
+            <Dialog open={isOpen} onOpenChange={closeModal}>
+                <DialogContent className="sm:max-w-[625px] overflow-y-auto items-start justify-start h-full sm:h-auto bg-white text-slate-700">
+                    <DialogHeader className='justify-start sm:flex hidden ml-auto mb-0 pb-0 '>
+                        <div className='mr-auto mb-6 rounded-full -mt-3 pr-1 w-20 flex items-start justify-start h-20'>
+                            <Image src="/gs3.svg" alt="Golden State Web Design" className='opacity-90 hidden sm:flex' width={65} height={65} />
                         </div>
-                        <div className="w-full">
-                            <Label className="text-slate-700 text-xs pb-3">Email<span className='text-red-500 pl-1'>*</span></Label>
-                            <Input
-                                id="email"
-                                placeholder="Your email"
-                                className="w-full bg-slate-50 border-slate-300"
-                                autoComplete='email'
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
+                        <DialogTitle>Let's Connect!</DialogTitle>
+                        <DialogDescription className='text-sm pt-3'>
+                            Let's connect! We're excited to hear about your project! Please fill out the form below and we'll get back to you as soon as we possible.
+                        </DialogDescription>
+                    </DialogHeader>
+
+                    <DialogHeader className='justify-center sm:hidden flex mx-auto pt-8 mb-0 pb-0'>
+                        <div className='mx-auto'>
+                            <Image src="/gs3.svg" alt="Golden State Web Design" className='opacity-90 lg:hidden ml-3 flex' width={55} height={55} />
                         </div>
-                        <div className="w-full">
-                            <Label className="text-slate-700 text-xs pb-3">Message</Label>
-                            <Textarea
-                                id="message"
-                                placeholder="Is there anything else we should know?"
-                                className="w-full bg-slate-50 border-slate-300 " 
-                                value={message}
-                                onChange={(e) => setMessage(e.target.value)}
-                            />
+                        <DialogTitle className='py-4 px-0'>How can we reach you?</DialogTitle>
+                    </DialogHeader>
+
+                    <form onSubmit={handleSubmit}>
+                        <div className="grid gap-5 py-4 pt-8">
+                            <div className="w-full">
+                                <Label className="text-slate-700 text-xs pb-3">Name<span className='text-red-500 pl-1'>*</span></Label>
+                                <Input
+                                    id="name"
+                                    placeholder="Your name"
+                                    className="w-full bg-slate-50 border-slate-300"
+                                    autoComplete='name'
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                />
+                            </div>
+                            <div className="w-full">
+                                <Label className="text-slate-700 text-xs pb-3">Email<span className='text-red-500 pl-1'>*</span></Label>
+                                <Input
+                                    id="email"
+                                    placeholder="Your email"
+                                    className="w-full bg-slate-50 border-slate-300"
+                                    autoComplete='email'
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                            </div>
+                            <div className="w-full">
+                                <Label className="text-slate-700 text-xs pb-3">Message</Label>
+                                <Textarea
+                                    id="message"
+                                    placeholder="Is there anything else we should know?"
+                                    className="w-full bg-slate-50 border-slate-300 " 
+                                    value={message}
+                                    onChange={(e) => setMessage(e.target.value)}
+                                />
+                            </div>
                         </div>
-                    </div>
-                    <DialogFooter className='pt-6'>
-                        <Button type="submit" variant="default" className='bg-slate-900 hover:bg-slate-900/90  w-full text-white'>
-                            {loading && <Spinner size="small" className='mr-2' />}
-                            {loading ? 'Processing...' : 'Submit'}
-                        </Button>
-                    </DialogFooter>
-                </form>
-            </DialogContent>
-        </Dialog>
+                        <DialogFooter className='pt-6'>
+                            <Button type="submit" variant="default" className='bg-slate-900 hover:bg-slate-900/90  w-full text-white'>
+                                {loading && <Spinner size="small" className='mr-2' />}
+                                {loading ? 'Processing...' : 'Submit'}
+                            </Button>
+                        </DialogFooter>
+                    </form>
+                </DialogContent>
+            </Dialog>
+        </Suspense>
     );
 }
